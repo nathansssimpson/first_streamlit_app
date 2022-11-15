2745d86ef1c5df41e8cf7359df713d95e87c5d8f
Nathan Simpson <77486677+nathansssimpson@users.noreply.github.com>
2022-11-15 14:56:52 +0000

    Update streamlit_app.py
    updated title, added bfast items with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mothers New Healthy Diner')
+
+streamlit.header('Breakfast Favs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.text('Designing first app with StreamLIT')
